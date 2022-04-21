921b938c8ce93e66f74b5cc3fade1c608491f899
Axerot93 <axerot93@github.com>
2022-04-21 13:02:38 +0200

    21/04/2022

--- corso/Guerrini/html/mavenproject1/src/main/java/com/mycompany/mavenproject1/ping/model/Greeting.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.mavenproject1.ping.model;
-
-public class Greeting {
-    public String message;
-
-    public Greeting(String message) {
-        this.message = message;
-    }
-}
